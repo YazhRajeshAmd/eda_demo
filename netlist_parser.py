27ef71a71913a07e8e25113bf4972838fc547fb0
YazhRajeshAmd <yazhrajesh25@gmail.com>
2026-01-06 10:58:14 -0800

    Update netlist_parser.py

--- netlist_parser.py
+++ netlist_parser.py
@@ -2,9 +2,10 @@ import numpy as np
 
 def load_netlist(path):
     nodes = {}
+    delays = []
     edges = []
 
-    with open(path, "r") as f:
+    with open(path) as f:
         mode = "nodes"
         for line in f:
             line = line.strip()
@@ -17,18 +18,10 @@ def load_netlist(path):
 
             if mode == "nodes":
                 name, delay = line.split()
-                nodes[name] = float(delay)
+                nodes[name] = len(nodes)
+                delays.append(float(delay))
             else:
                 src, dst = line.split()
-                edges.append((src, dst))
+                edges.append((nodes[src], nodes[dst]))
 
-    node_list = list(nodes.keys())
-    node_index = {n: i for i, n in enumerate(node_list)}
-
-    delays = np.array([nodes[n] for n in node_list])
-
-    adj = np.zeros((len(node_list), len(node_list)))
-    for s, d in edges:
-        adj[node_index[d], node_index[s]] = 1.0
-
-    return adj, delays, node_list
+    return edges, np.array(delays, dtype=np.float32), list(nodes.keys())
